9c0aa8d2d5188540cdb8f6ec933ae64cd93b4603
FENGZEYOU123 <35758413+FENGZEYOU123@users.noreply.github.com>
2021-04-27 15:35:26 +0800

    random add delete

--- app/src/main/java/com/yfz/statisticsview/ColumnBarChartView.java
+++ app/src/main/java/com/yfz/statisticsview/ColumnBarChartView.java
@@ -1,10 +1,12 @@
 package com.yfz.statisticsview;
 
 import android.content.Context;
+import android.content.res.TypedArray;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Rect;
+import android.graphics.RectF;
 import android.util.AttributeSet;
 import android.view.View;
 import androidx.annotation.Nullable;
@@ -21,59 +23,87 @@ public class ColumnBarChartView extends View {
     //记录绘制数据，key名字,value数量
     private ArrayList<ColumnDataFrom> mArray=new ArrayList<>();
     //柱状间距
-    private int mMargin=10;
+    private int mMargin=20;
     //柱状宽度
-    private int mWidth=0;
+    private float mWidth=0;
     //柱状高度
-    private int mHeight=0;
+    private float mHeight=0;
     //柱状位置Rect
-    private Rect mRect = new Rect();
+    private RectF mRectF = new RectF();
+    //柱状名字显示
+    private boolean mDisplayName=false;
     public ColumnBarChartView(Context context) {
         super(context);
         initial();
     }
     public ColumnBarChartView(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
-        initial();
-    }
-    public ColumnBarChartView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
-        super(context, attrs, defStyleAttr);
+        TypedArray typedArray=context.obtainStyledAttributes(attrs, R.styleable.ColumnBarChartView);
+        //设置柱状的间距
+        mMargin=typedArray.getInteger(R.styleable.ColumnBarChartView_ColumnBarChart_margin,mMargin);
+        mDisplayName=typedArray.getBoolean(R.styleable.ColumnBarChartView_ColumnBarChart_displayName,mDisplayName);
         initial();
     }
 
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+
+    }
+
     private void initial(){
-        mPaint.setAntiAlias(true);
+        mPaint.setAntiAlias(true); //抗锯齿
         mPaint.setStyle(Paint.Style.FILL);
         setWillNotDraw(false);
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
-        mWidth  = (int) ( getWidth() - (mMargin*(getSize()-1)) ) / getSize();
-        mHeight = (int) ( getHeight() / getNumber() );
+        mWidth  = ( getWidth() - (mMargin*(getSize()-1)) ) / getSize(); //计算每个柱状宽度
+        mHeight = ( getHeight() / getNumber() ); //计算柱状数值的平均高度值
         for(int i=0;i<mArray.size();i++){
-            mPaint.setColor(mArray.get(i).mColor);
-            mRect.left= i*(mWidth+mMargin);
-            mRect.right= mRect.left+mWidth;
-            mRect.bottom=getHeight();
-            mRect.top=(int) ( mHeight*mArray.get(i).mNumber );
-            canvas.drawRect(mRect,mPaint);
+            if(mDisplayName){ //如果设置了展示名字，则绘制名字
+                onDrawName(canvas,i); //画柱状名字
+            }
+                onDrawColumn(canvas,i); //画柱状图形
         }
     }
 
+    //绘制-柱状名字
+    private void onDrawName(Canvas canvas,int i){
+
+    }
+    //绘制-柱状图形
+    private void onDrawColumn(Canvas canvas,int i){
+        mPaint.setColor(mArray.get(i).mColor); //柱状颜色
+        mRectF.left= i*(mWidth+mMargin);
+        mRectF.right= mRectF.left+mWidth;
+        mRectF.bottom=getHeight();
+        mRectF.top= mRectF.bottom-mHeight*mArray.get(i).mNumber ; //柱状高度=数值的平均高度值*柱状数值
+        canvas.drawRect(mRectF,mPaint);
+    }
+
+    /**
+     * 是否显示柱状名称
+     */
+    public void displayName(boolean isDisplay){
+        mDisplayName=isDisplay;
+    }
     /**
      * 向外提供添加数据的方法,如果数据名字一样则被替换
      */
     public void addColumnData(ColumnDataFrom columnDataFrom){
         if(null != mArray){
-            mArray.add(columnDataFrom);
-            refreshUI();
+            if(!mArray.contains(columnDataFrom)) {
+                mArray.add(columnDataFrom);
+                refreshUI();
+            }
         }
     }
     /**
      * 向外提供删除数据的方法
      */
-    public void removeColumnData(String name){
+    public void deleteColumnData(String name){
         if(null != mArray){
             if(mArray.contains(name)){
                 mArray.remove(name);
@@ -81,7 +111,17 @@ public class ColumnBarChartView extends View {
             }
         }
     }
-
+    /**
+     * 向外提供删除数据的方法
+     */
+    public void deleteColumnData(int index){
+        if(null != mArray){
+            if(mArray.size()>index){
+                mArray.remove(index);
+                refreshUI();
+            }
+        }
+    }
     /**
      * 柱状数据表单
      */
@@ -99,16 +139,24 @@ public class ColumnBarChartView extends View {
     private void refreshUI(){
         this.invalidate();
     }
-    //获取总柱状数量
-    private int getSize(){
+
+    /**
+     * 获取柱状数量
+     * @return
+     */
+    public int getSize(){
         if(null!=mArray){
             return mArray.size();
         }else {
             return 0;
         }
     }
-    //获取所有数值总和
-    private float getNumber(){
+
+    /**
+     * 获取所有柱状的数值总数
+     * @return
+     */
+    public float getNumber(){
         float totalNumber=0;
         for(ColumnDataFrom columnDataFrom : mArray){
             totalNumber= totalNumber+ columnDataFrom.mNumber;
--- app/src/main/java/com/yfz/statisticsview/MainActivity.java
+++ app/src/main/java/com/yfz/statisticsview/MainActivity.java
@@ -4,6 +4,10 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.graphics.Color;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
     private ColumnBarChartView mColumnBarChartView;
@@ -14,10 +18,38 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         initial();
     }
-    private void initial(){
-        mColumnBarChartView=findViewById(R.id.columnBarChartView);
-        mColumnBarChartView.addColumnData(new ColumnBarChartView.ColumnDataFrom("不及格",5, Color.YELLOW));
-        mColumnBarChartView.addColumnData(new ColumnBarChartView.ColumnDataFrom("及格",15,Color.RED));
-        mColumnBarChartView.addColumnData(new ColumnBarChartView.ColumnDataFrom("优秀",10,Color.BLUE));
+
+    private void initial() {
+        mColumnBarChartView = findViewById(R.id.columnBarChartView);
+        mColumnBarChartView.addColumnData(new ColumnBarChartView.ColumnDataFrom("不及格", 5, Color.YELLOW));
+        mColumnBarChartView.addColumnData(new ColumnBarChartView.ColumnDataFrom("及格", 15, Color.RED));
+        mColumnBarChartView.addColumnData(new ColumnBarChartView.ColumnDataFrom("优秀", 10, Color.BLUE));
+    }
+
+    public void addRandomData(View view) {
+        int number = (int)(Math.random()*30+5);
+        mColumnBarChartView.addColumnData(new ColumnBarChartView.ColumnDataFrom("随机的 " + number, number, getRandColorCode()));
+    }
+
+    public void deleteRandomData(View view) {
+        Random random = new Random();
+        int number =  random.nextInt(mColumnBarChartView.getSize()-1);
+        mColumnBarChartView.deleteColumnData(number);
+    }
+
+    private static int getRandColorCode() {
+        String r, g, b;
+        Random random = new Random();
+        r = Integer.toHexString(random.nextInt(256)).toUpperCase();
+        g = Integer.toHexString(random.nextInt(256)).toUpperCase();
+        b = Integer.toHexString(random.nextInt(256)).toUpperCase();
+
+        r = r.length() == 1 ? "0" + r : r;
+        g = g.length() == 1 ? "0" + g : g;
+        b = b.length() == 1 ? "0" + b : b;
+
+        int a =  Color.parseColor("#"+r + g + b);
+        return  a;
+
     }
 }
